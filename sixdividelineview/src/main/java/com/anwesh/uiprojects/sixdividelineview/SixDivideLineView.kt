8b76fd94267bacca6b272ce611929e9db8bc91bb
Anwesh43 <anweshthecool0@gmail.com>
2020-08-15 03:59:52 +0530

    adding constants and libraries

--- sixdividelineview/src/main/java/com/anwesh/uiprojects/sixdividelineview/SixDivideLineView.kt
+++ sixdividelineview/src/main/java/com/anwesh/uiprojects/sixdividelineview/SixDivideLineView.kt
@@ -3,3 +3,17 @@ package com.anwesh.uiprojects.sixdividelineview
 /**
  * Created by anweshmishra on 15/08/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.*
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<String> = arrayOf("#3F51B5", "#4CAF50", "#F44336", "#2196F3", "#009688")
+val parts : Int = 2
+val scGap : Float = 0.02f / parts
+val strokeFactor : Int = 90
+val sizeFactor : Float = 4.8f
+val foreColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
